7b7586c09a5f8f98b918d8c2a71198fcebdae200
mastekator <akmiskam@yandex.ru>
2019-11-11 14:47:09 +0300

    страница анонсы книг

--- wp-content/themes/storefront-child/template-parts/announcement.php
+++ wp-content/themes/storefront-child/template-parts/announcement.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Template Name: announcement
+Template Name: news
 Template Post Type: post, page, product
 */
 ?>
@@ -8,10 +8,124 @@ Template Post Type: post, page, product
 <?php get_header(); ?>
 
 
-<div class="container pt-5 pb-5 popular">
+<div class="container news">
     <div class="row">
-        <div class="col-12">
-            <h1>Страница анонсы книг</h1>
+        <div class="container announcement">
+            <div class="row">
+                <div class="col-12"><h2 class="page-title"><?php the_title() ?></h2></div>
+            </div>
+            <div class="row announcement-row">
+                <div class="col-lg-6 col-12">
+                    <div class="row">
+                        <div class="col-lg-6 col-12 m-auto">
+                            <a href="#">
+                                <div class="announcement-img">
+                                    <img src="/wp-content/themes/storefront-child/images/announcement-example.jpg"
+                                         alt="">
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-lg-6 col-12">
+                            <h3 class="announcement-header">
+                                Аудиокнига “Девушка в цепях”
+                            </h3>
+                            <p class="announcement-content">
+                                🎧 Совсем скоро! Аудиокнига "Девушка в цепях" на Литрес!
+                            </p>
+                            <a href="#" class="announcement-btn">Подробнее</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-12">
+                    <div class="row">
+                        <div class="col-lg-6 col-12 m-auto">
+                            <a href="#">
+                                <div class="announcement-img">
+                                    <img src="/wp-content/themes/storefront-child/images/announcement-example.jpg"
+                                         alt="">
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-lg-6 col-12">
+                            <h3 class="announcement-header">
+                                Аудиокнига “Девушка в цепях”
+                            </h3>
+                            <p class="announcement-content">
+                                🎧 Совсем скоро! Аудиокнига "Девушка в цепях" на Литрес!
+                            </p>
+                            <a href="#" class="announcement-btn">Подробнее</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-12">
+                    <div class="row">
+                        <div class="col-lg-6 col-12 m-auto">
+                            <a href="#">
+                                <div class="announcement-img">
+                                    <img src="/wp-content/themes/storefront-child/images/announcement-example.jpg"
+                                         alt="">
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-lg-6 col-12">
+                            <h3 class="announcement-header">
+                                Аудиокнига “Девушка в цепях”
+                            </h3>
+                            <p class="announcement-content">
+                                🎧 Совсем скоро! Аудиокнига "Девушка в цепях" на Литрес!
+                            </p>
+                            <a href="#" class="announcement-btn">Подробнее</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-12">
+                    <div class="row">
+                        <div class="col-lg-6 col-12 m-auto">
+                            <a href="#">
+                                <div class="announcement-img">
+                                    <img src="/wp-content/themes/storefront-child/images/announcement-example.jpg"
+                                         alt="">
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-lg-6 col-12">
+                            <h3 class="announcement-header">
+                                Аудиокнига “Девушка в цепях”
+                            </h3>
+                            <p class="announcement-content">
+                                🎧 Совсем скоро! Аудиокнига "Девушка в цепях" на Литрес!
+                            </p>
+                            <a href="#" class="announcement-btn">Подробнее</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-12">
+                    <div class="row">
+                        <div class="col-lg-6 col-12 m-auto">
+                            <a href="">
+                                <div class="announcement-img">
+                                    <img src="/wp-content/themes/storefront-child/images/announcement-example.jpg"
+                                         alt="">
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-lg-6 col-12">
+                            <h3 class="announcement-header">
+                                Аудиокнига “Девушка в цепях”
+                            </h3>
+                            <p class="announcement-content">
+                                🎧 Совсем скоро! Аудиокнига "Девушка в цепях" на Литрес!
+                            </p>
+                            <a href="#" class="announcement-btn">Подробнее</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col text-center">
+                    <div class="load-more">Загрузить еще</div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
