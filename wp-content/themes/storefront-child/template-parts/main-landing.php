46d02c87566352ea087d3a5aec95e90e20e6e4ea
mastekator <akmiskam@yandex.ru>
2019-11-11 13:57:37 +0300

    готовая верстка главной

--- wp-content/themes/storefront-child/template-parts/main-landing.php
+++ wp-content/themes/storefront-child/template-parts/main-landing.php
@@ -263,7 +263,61 @@ Template Post Type: post, page, product
                 </div>
             </div>
             <div class="row">
-                <div class="col text-center"><a class="blog__link" href="#">Смотреть все анонсы</a></div>
+                <div class="col text-center"><a class="blog__link" href="#">Смотреть все посты</a></div>
+            </div>
+        </div>
+        <div class="container news-n-events">
+            <div class="row news-n-events-hr-block">
+                <div class="col-12">
+                    <p class="news-n-events-hr-block__text">Новости и события</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-12">
+                    <div class="news-n-events-card">
+                        <div class="news-n-events-card-body">
+                            <p class="news-n-events-card__date">31.10.2019</p>
+                            <p class="news-n-events-card__text">Скоро будем обсуждать обложку для Фервернской
+                                истории с
+                                Ириной Косулиной </p>
+                            <a href="#" class="news-n-events-card__link">Подробнее</a>
+                            <a href="#" class="news-n-events-card__author">Марина Эльденбер</a>
+                        </div>
+                        <img class="news-n-events-card__avatar"
+                             src="/wp-content/themes/storefront-child/images/avatar.jpg" alt="">
+                    </div>
+                </div>
+                <div class="col-lg-4 col-12">
+                    <div class="news-n-events-card">
+                        <div class="news-n-events-card-body">
+                            <p class="news-n-events-card__date">31.10.2019</p>
+                            <p class="news-n-events-card__text">Скоро будем обсуждать обложку для Фервернской
+                                истории с
+                                Ириной Косулиной </p>
+                            <a href="#" class="news-n-events-card__link">Подробнее</a>
+                            <a href="#" class="news-n-events-card__author">Марина Эльденбер</a>
+                        </div>
+                        <img class="news-n-events-card__avatar"
+                             src="/wp-content/themes/storefront-child/images/avatar.jpg" alt="">
+                    </div>
+                </div>
+                <div class="col-lg-4 col-12">
+                    <div class="news-n-events-card">
+                        <div class="news-n-events-card-body">
+                            <p class="news-n-events-card__date">31.10.2019</p>
+                            <p class="news-n-events-card__text">Скоро будем обсуждать обложку для Фервернской
+                                истории с
+                                Ириной Косулиной </p>
+                            <a href="#" class="news-n-events-card__link">Подробнее</a>
+                            <a href="#" class="news-n-events-card__author">Марина Эльденбер</a>
+                        </div>
+                        <img class="news-n-events-card__avatar"
+                             src="/wp-content/themes/storefront-child/images/avatar.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col text-center"><a class="news-n-events__link" href="#">Смотреть все новости</a></div>
             </div>
         </div>
 
