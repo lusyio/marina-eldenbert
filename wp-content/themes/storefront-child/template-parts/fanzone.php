99425b19bbe5acd53e995acde7516144717a260a
mastekator <akmiskam@yandex.ru>
2019-11-11 10:45:13 +0300

    шаблоны страниц

---
+++ wp-content/themes/storefront-child/template-parts/fanzone.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Template Name: fanzone
+Template Post Type: post, page, product
+*/
+?>
+
+<?php get_header(); ?>
+
+
+<div class="container pt-5 pb-5 popular">
+    <div class="row">
+        <div class="col-12">
+            <h1>Страница фанзоны</h1>
+        </div>
+    </div>
+</div>
+
+
+<?php get_footer(); ?>
