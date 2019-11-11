180221a47eb139235e8d2cba53579a8bf32b5c17
mastekator <akmiskam@yandex.ru>
2019-11-11 12:57:31 +0300

    авторский блог и анонсы на главной

--- wp-content/themes/storefront-child/template-parts/main-landing.php
+++ wp-content/themes/storefront-child/template-parts/main-landing.php
@@ -98,53 +98,58 @@ Template Post Type: post, page, product
 
 <div class="container-related">
     <div class="container position-relative">
-        <div class="container-related__prev"><img src="/wp-content/themes/storefront-child/svg/related-next.svg" alt=""></div>
-        <div class="container-related__next"><img src="/wp-content/themes/storefront-child/svg/related-next.svg" alt=""></div>
+        <div class="container-related__prev"><img src="/wp-content/themes/storefront-child/svg/related-next.svg" alt="">
+        </div>
+        <div class="container-related__next"><img src="/wp-content/themes/storefront-child/svg/related-next.svg" alt="">
+        </div>
         <div class="row">
             <div class="col-lg-10 offset-lg-1 col-12 offset-0 text-center">
                 <div class="swiper-container-related">
                     <div class="swiper-wrapper">
                         <div class="swiper-slide">
-                            <div class="related-img-container">
-                                <img src="/wp-content/themes/storefront-child/images/related-example.jpg" alt="">
-                                <span class="related-img-container__price">149 Р</span>
-                                <p class="related-img-container__header">Поющая для дракона</p>
-                            </div>
+                            <a href="#">
+                                <div class="related-img-container">
+                                    <img src="/wp-content/themes/storefront-child/images/related-example.jpg" alt="">
+                                    <span class="related-img-container__price">149 Р</span>
+                                    <p class="related-img-container__header">Поющая для дракона</p>
+                                </div>
+                            </a>
                         </div>
                         <div class="swiper-slide">
-                            <div class="related-img-container">
-                                <img src="/wp-content/themes/storefront-child/images/related-example.jpg" alt="">
-                                <span class="related-img-container__price">149 Р</span>
-                                <p class="related-img-container__header">Поющая для дракона</p>
-                            </div>
+                            <a href="#">
+                                <div class="related-img-container">
+                                    <img src="/wp-content/themes/storefront-child/images/related-example.jpg" alt="">
+                                    <span class="related-img-container__price">149 Р</span>
+                                    <p class="related-img-container__header">Поющая для дракона</p>
+                                </div>
+                            </a>
                         </div>
                         <div class="swiper-slide">
-                            <div class="related-img-container">
-                                <img src="/wp-content/themes/storefront-child/images/related-example.jpg" alt="">
-                                <span class="related-img-container__price">149 Р</span>
-                                <p class="related-img-container__header">Поющая для дракона</p>
-                            </div>
+                            <a href="#">
+                                <div class="related-img-container">
+                                    <img src="/wp-content/themes/storefront-child/images/related-example.jpg" alt="">
+                                    <span class="related-img-container__price">149 Р</span>
+                                    <p class="related-img-container__header">Поющая для дракона</p>
+                                </div>
+                            </a>
                         </div>
                         <div class="swiper-slide">
-                            <div class="related-img-container">
-                                <img src="/wp-content/themes/storefront-child/images/related-example.jpg" alt="">
-                                <span class="related-img-container__price">149 Р</span>
-                                <p class="related-img-container__header">Поющая для дракона</p>
-                            </div>
+                            <a href="#">
+                                <div class="related-img-container">
+                                    <img src="/wp-content/themes/storefront-child/images/related-example.jpg" alt="">
+                                    <span class="related-img-container__price">149 Р</span>
+                                    <p class="related-img-container__header">Поющая для дракона</p>
+                                </div>
+                            </a>
                         </div>
                         <div class="swiper-slide">
-                            <div class="related-img-container">
-                                <img src="/wp-content/themes/storefront-child/images/related-example.jpg" alt="">
-                                <span class="related-img-container__price">149 Р</span>
-                                <p class="related-img-container__header">Поющая для дракона</p>
-                            </div>
-                        </div>
-                        <div class="swiper-slide">
-                            <div class="related-img-container">
-                                <img src="/wp-content/themes/storefront-child/images/related-example.jpg" alt="">
-                                <span class="related-img-container__price">149 Р</span>
-                                <p class="related-img-container__header">Поющая для дракона</p>
-                            </div>
+                            <a href="#">
+                                <div class="related-img-container">
+                                    <img src="/wp-content/themes/storefront-child/images/related-example.jpg" alt="">
+                                    <span class="related-img-container__price">149 Р</span>
+                                    <p class="related-img-container__header">Поющая для дракона</p>
+                                </div>
+                            </a>
                         </div>
                     </div>
                 </div>
@@ -153,5 +158,113 @@ Template Post Type: post, page, product
     </div>
 </div>
 
+<div class="container">
+    <div class="row">
+        <div class="container announcement">
+            <div class="row announcement-hr-block">
+                <div class="col-12">
+                    <p class="announcement-hr-block__text">Анонсы</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-6 col-12">
+                    <div class="row">
+                        <div class="col-lg-6 col-12 m-auto">
+                            <a href="#">
+                                <div class="announcement-img">
+                                    <img src="/wp-content/themes/storefront-child/images/announcement-example.jpg"
+                                         alt="">
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-lg-6 col-12">
+                            <h3 class="announcement-header">
+                                Аудиокнига “Девушка в цепях”
+                            </h3>
+                            <p class="announcement-content">
+                                🎧 Совсем скоро! Аудиокнига "Девушка в цепях" на Литрес!
+                            </p>
+                            <a href="#" class="announcement-btn">Подробнее</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-12">
+                    <div class="row">
+                        <div class="col-lg-6 col-12 m-auto">
+                            <a href="">
+                                <div class="announcement-img">
+                                    <img src="/wp-content/themes/storefront-child/images/announcement-example.jpg"
+                                         alt="">
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-lg-6 col-12">
+                            <h3 class="announcement-header">
+                                Аудиокнига “Девушка в цепях”
+                            </h3>
+                            <p class="announcement-content">
+                                🎧 Совсем скоро! Аудиокнига "Девушка в цепях" на Литрес!
+                            </p>
+                            <a href="#" class="announcement-btn">Подробнее</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col text-center"><a class="announcement__link" href="#">Смотреть все анонсы</a></div>
+            </div>
+        </div>
+        <div class="container blog">
+            <div class="row blog-hr-block">
+                <div class="col-12">
+                    <p class="blog-hr-block__text">Авторский блог</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-6 col-12">
+                    <div class="blog-card">
+                        <div class="blog-card__header">
+                            <a href="#">
+                                <img class="blog-card__img"
+                                     src="/wp-content/themes/storefront-child/images/blog-example.jpg"
+                                     alt="">
+                            </a>
+                        </div>
+                        <div class="blog-card__body">
+                            <p class="blog-card__date">31.10.2019</p>
+                            <p class="blog-card__text">Название найдено! Из предложенных вариантов издательство
+                                остановилось
+                                на
+                                варианте, предложенном Оксаной Тимофеевой 😉 Итак, вторая часть будет называться...
+                            </p>
+                            <a class="blog-card__link" href="#">Подробнее</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-12">
+                    <div class="blog-card">
+                        <div class="blog-card__header">
+                            <a href="#">
+                                <img class="blog-card__img"
+                                     src="/wp-content/themes/storefront-child/images/blog-example.jpg"
+                                     alt="">
+                            </a>
+                        </div>
+                        <div class="blog-card__body">
+                            <p class="blog-card__date">31.10.2019</p>
+                            <p class="blog-card__text">Название найдено! Из предложенных вариантов издательство
+                                остановилось
+                                на
+                                варианте, предложенном Оксаной Тимофеевой 😉 Итак, вторая часть будет называться...
+                            </p>
+                            <a class="blog-card__link" href="#">Подробнее</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col text-center"><a class="blog__link" href="#">Смотреть все анонсы</a></div>
+            </div>
+        </div>
 
-<?php get_footer(); ?>
+        <?php get_footer(); ?>
