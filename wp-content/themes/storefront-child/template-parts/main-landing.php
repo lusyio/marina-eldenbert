0e2b99079aeeec065e7f488f101f75298f07fcd7
mastekator <akmiskam@yandex.ru>
2019-11-12 11:06:48 +0300

    добавил ховер на анонсы

--- wp-content/themes/storefront-child/template-parts/main-landing.php
+++ wp-content/themes/storefront-child/template-parts/main-landing.php
@@ -58,7 +58,6 @@ Template Post Type: post, page, product
                             </div>
                         </div>
                     </div>
-
                     <div class="swiper-slide">
                         <div class="row">
                             <div class="col-12 col-lg-4 offset-lg-1 offset-0">
@@ -68,32 +67,41 @@ Template Post Type: post, page, product
                                 </div>
                             </div>
                             <div class="col-12 col-lg-7">
-                                <h2 class="popular-header">Популярное</h2>
-                                <h2 class="popular-title">Поющая для дракона. Книга 3.
-                                    Между двух огней
-                                </h2>
-                                <p class="popular-content">
-                                    Он считает, что я в его власти, но он ошибается. Мир иртханов не прощает слабости, а
-                                    значит, мне
-                                    придется научиться играть по его правилам. На его условиях, по законам, которые
-                                    ближе к
-                                    звериным, чем к человеческим. Чем закончится наше противостояние? Я не знаю. Знаю
-                                    только, что
-                                    буду петь. Даже если сердце в огне. Он считает, что я в его власти, но он ошибается.
-                                    Мир
-                                    иртханов не прощает слабости, а значит, мне придется научиться играть по его
-                                    правилам.
-                                </p>
                                 <div class="row">
-                                    <div class="col-lg-5 col-12">
-                                        <div class="popular-btn">
-                                            Подробнее
+                                    <div class="col-lg-11 offset-lg-1 offset-0 col-12">
+                                        <h2 class="popular-header">Популярное</h2>
+                                        <h2 class="popular-title">Поющая для дракона. Книга 3.
+                                            Между двух огней
+                                        </h2>
+                                        <p class="popular-content">
+                                            Он считает, что я в его власти, но он ошибается. Мир иртханов не прощает
+                                            слабости, а
+                                            значит, мне
+                                            придется научиться играть по его правилам. На его условиях, по законам,
+                                            которые
+                                            ближе к
+                                            звериным, чем к человеческим. Чем закончится наше противостояние? Я не знаю.
+                                            Знаю
+                                            только, что
+                                            буду петь. Даже если сердце в огне. Он считает, что я в его власти, но он
+                                            ошибается.
+                                            Мир
+                                            иртханов не прощает слабости, а значит, мне придется научиться играть по его
+                                            правилам.
+                                        </p>
+                                        <div class="row">
+                                            <div class="col-lg-5 col-12">
+                                                <div class="popular-btn">
+                                                    Подробнее
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
+
                 </div>
                 <div class="swiper-pagination popular-pagination">
                 </div>
@@ -177,7 +185,7 @@ Template Post Type: post, page, product
             <div class="row">
                 <div class="col-lg-6 col-12">
                     <div class="row">
-                        <div class="col-lg-6 col-12 m-auto">
+                        <div class="col-lg-6 col-12">
                             <a href="#">
                                 <div class="announcement-img">
                                     <img src="/wp-content/themes/storefront-child/images/announcement-example.jpg"
@@ -198,7 +206,7 @@ Template Post Type: post, page, product
                 </div>
                 <div class="col-lg-6 col-12">
                     <div class="row">
-                        <div class="col-lg-6 col-12 m-auto">
+                        <div class="col-lg-6 col-12">
                             <a href="">
                                 <div class="announcement-img">
                                     <img src="/wp-content/themes/storefront-child/images/announcement-example.jpg"
