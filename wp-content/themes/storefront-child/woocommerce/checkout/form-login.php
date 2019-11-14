4f675c4e854ce37cc1f130c76a1231eea05dbe0b
mastekator <akmiskam@yandex.ru>
2019-11-14 13:31:11 +0300

    логин при оформлении заказа

--- wp-content/themes/storefront-child/woocommerce/checkout/form-login.php
+++ wp-content/themes/storefront-child/woocommerce/checkout/form-login.php
@@ -15,22 +15,28 @@
  * @version 3.4.0
  */
 
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
-if ( is_user_logged_in() || 'no' === get_option( 'woocommerce_enable_checkout_login_reminder' ) ) {
-	return;
+if (is_user_logged_in() || 'no' === get_option('woocommerce_enable_checkout_login_reminder')) {
+    return;
 }
 
 ?>
-<div class="woocommerce-form-login-toggle">
-	<?php wc_print_notice( apply_filters( 'woocommerce_checkout_login_message', esc_html__( 'Returning customer?', 'woocommerce' ) ) . ' <a href="#" class="showlogin">' . esc_html__( 'Click here to login', 'woocommerce' ) . '</a>', 'notice' ); ?>
-</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 offset-lg-3 col-12 offset-0">
+                <div class="woocommerce-form-login-toggle">
+                    <?php wc_print_notice(apply_filters('woocommerce_checkout_login_message', esc_html__('Returning customer?', 'woocommerce')) . ' <a href="#" class="showlogin">' . esc_html__('Click here to login', 'woocommerce') . '</a>', 'notice'); ?>
+                </div>
+            </div>
+        </div>
+    </div>
 <?php
 
 woocommerce_login_form(
-	array(
-		'message'  => esc_html__( 'If you have shopped with us before, please enter your details below. If you are a new customer, please proceed to the Billing section.', 'woocommerce' ),
-		'redirect' => wc_get_page_permalink( 'checkout' ),
-		'hidden'   => true,
-	)
+    array(
+        'message' => esc_html__('If you have shopped with us before, please enter your details below. If you are a new customer, please proceed to the Billing section.', 'woocommerce'),
+        'redirect' => wc_get_page_permalink('checkout'),
+        'hidden' => true,
+    )
 );
