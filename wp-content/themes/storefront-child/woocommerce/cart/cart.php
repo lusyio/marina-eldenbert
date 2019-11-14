924cee0ae38431b307104c7be10e062ebcc2d916
mastekator <akmiskam@yandex.ru>
2019-11-14 14:56:42 +0300

    поправил корзину

--- wp-content/themes/storefront-child/woocommerce/cart/cart.php
+++ wp-content/themes/storefront-child/woocommerce/cart/cart.php
@@ -22,7 +22,7 @@ do_action( 'woocommerce_before_cart' ); ?>
 <form class="woocommerce-cart-form" action="<?php echo esc_url( wc_get_cart_url() ); ?>" method="post">
     <?php do_action( 'woocommerce_before_cart_table' ); ?>
 
-    <table class="shop_table shop_table_responsive cart woocommerce-cart-form__contents mt-5" cellspacing="0">
+    <table class="shop_table cart woocommerce-cart-form__contents mt-5" cellspacing="0">
         <tbody>
         <?php do_action( 'woocommerce_before_cart_contents' ); ?>
 
