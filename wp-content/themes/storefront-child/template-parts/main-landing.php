4e07d07a11fffce7042f0c649dff2846cad25a0f
mastekator <akmiskam@yandex.ru>
2019-11-15 17:02:17 +0300

    вывод на главную новинок и бестселлеров

--- wp-content/themes/storefront-child/template-parts/main-landing.php
+++ wp-content/themes/storefront-child/template-parts/main-landing.php
@@ -14,46 +14,53 @@ Template Post Type: post, page, product
 
             <div class="swiper-container-popular">
                 <div class="swiper-wrapper">
-
-                    <div class="swiper-slide">
-                        <div class="row">
-                            <div class="col-12 col-lg-4 offset-lg-1 offset-0">
-                                <h2 class="popular-header d-block d-lg-none text-center">Популярное</h2>
-                                <div class="d-table m-auto">
-                                    <div class="popular-img-container">
-                                        <img src="/wp-content/themes/storefront-child/images/popular-example.jpg"
-                                             alt="">
-                                        <span class="popular-img-container__price">149 Р</span>
+                    <?php
+                    $tags = array(
+                        'new',
+                        'bestseller'
+                    );
+                    $query = new WC_Product_Query(array(
+                        'status' => 'publish',
+                        'orderby' => 'date',
+                        'order' => 'DESC',
+                        'limit' => 5,
+                        'tax_query' => array(
+                            array(
+                                'taxonomy' => 'product_tag',
+                                'field' => 'slug',
+                                'terms' => $tags,
+                            )
+                        )
+                    ));
+                    $terms = get_the_terms( get_the_ID(), 'product_cat' );
+                    $products = $query->get_products();
+                    foreach ($products as $product): ?>
+                        <div class="swiper-slide">
+                            <div class="row">
+                                <div class="col-12 col-lg-4 offset-lg-1 offset-0">
+                                    <h2 class="popular-header d-block d-lg-none text-center">Популярное</h2>
+                                    <div class="d-table m-auto">
+                                        <div class="popular-img-container">
+                                            <?php echo $product->get_image(385); ?>
+                                            <span class="popular-img-container__price"><?php echo $product->get_price_html(); ?></span>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="col-12 col-lg-7">
-                                <div class="row">
-                                    <div class="col-lg-11 offset-lg-1 offset-0 col-12">
-                                        <h2 class="popular-header d-lg-block d-none">Популярное</h2>
-                                        <h2 class="popular-title">Поющая для дракона. Книга 3.
-                                            Между двух огней
-                                        </h2>
-                                        <p class="popular-content">
-                                            Он считает, что я в его власти, но он ошибается. Мир иртханов не прощает
-                                            слабости, а
-                                            значит, мне
-                                            придется научиться играть по его правилам. На его условиях, по законам,
-                                            которые
-                                            ближе к
-                                            звериным, чем к человеческим. Чем закончится наше противостояние? Я не знаю.
-                                            Знаю
-                                            только, что
-                                            буду петь. Даже если сердце в огне. Он считает, что я в его власти, но он
-                                            ошибается.
-                                            Мир
-                                            иртханов не прощает слабости, а значит, мне придется научиться играть по его
-                                            правилам.
-                                        </p>
-                                        <div class="row">
-                                            <div class="col-lg-5 col-12">
-                                                <div class="popular-btn">
-                                                    Подробнее
+                                <div class="col-12 col-lg-7">
+                                    <div class="row">
+                                        <div class="col-lg-11 offset-lg-1 offset-0 col-12">
+                                            <h2 class="popular-header d-lg-block d-none"><?php echo $product->get_tags(); ?></h2>
+                                            <h2 class="popular-title"><?php echo $product->get_name(); ?>
+                                            </h2>
+                                            <p class="popular-content">
+                                                <?php echo $product->get_short_description(); ?>
+                                            </p>
+                                            <div class="row">
+                                                <div class="col-lg-5 col-12">
+                                                    <a href="<?php echo $product->get_permalink(); ?>"
+                                                       class="popular-btn">
+                                                        Подробнее
+                                                    </a>
                                                 </div>
                                             </div>
                                         </div>
@@ -61,55 +68,7 @@ Template Post Type: post, page, product
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="swiper-slide">
-                        <div class="row">
-                            <div class="col-12 col-lg-4 offset-lg-1 offset-0">
-                                <h2 class="popular-header d-block d-lg-none text-center">Популярное</h2>
-                                <div class="d-table m-auto">
-                                    <div class="popular-img-container">
-                                        <img src="/wp-content/themes/storefront-child/images/popular-example.jpg"
-                                             alt="">
-                                        <span class="popular-img-container__price">149 Р</span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-12 col-lg-7">
-                                <div class="row">
-                                    <div class="col-lg-11 offset-lg-1 offset-0 col-12">
-                                        <h2 class="popular-header d-lg-block d-none">Популярное</h2>
-                                        <h2 class="popular-title">Поющая для дракона. Книга 3.
-                                            Между двух огней
-                                        </h2>
-                                        <p class="popular-content">
-                                            Он считает, что я в его власти, но он ошибается. Мир иртханов не прощает
-                                            слабости, а
-                                            значит, мне
-                                            придется научиться играть по его правилам. На его условиях, по законам,
-                                            которые
-                                            ближе к
-                                            звериным, чем к человеческим. Чем закончится наше противостояние? Я не знаю.
-                                            Знаю
-                                            только, что
-                                            буду петь. Даже если сердце в огне. Он считает, что я в его власти, но он
-                                            ошибается.
-                                            Мир
-                                            иртханов не прощает слабости, а значит, мне придется научиться играть по его
-                                            правилам.
-                                        </p>
-                                        <div class="row">
-                                            <div class="col-lg-5 col-12">
-                                                <div class="popular-btn">
-                                                    Подробнее
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
+                    <?php endforeach; ?>
                 </div>
                 <div class="swiper-pagination popular-pagination">
                 </div>
