4f4560d3c0f7bb1f5e6a6eb60dbb1d68627e4192
mastekator <akmiskam@yandex.ru>
2019-11-18 15:57:13 +0300

    Update page.php

--- wp-content/themes/storefront-child/page.php
+++ wp-content/themes/storefront-child/page.php
@@ -11,30 +11,19 @@
  */
 
 get_header(); ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 position-relative">
+                <div class="row">
+                    <div class="col-12"><h2 class="page-title"><?php the_title() ?></h2></div>
+                </div>
+                <?php
 
-	<div id="primary" class="content-area col-sm-12 col-lg-9">
-		<main id="main" class="site-main" role="main">
+                the_content();
 
-			<?php
-			while ( have_posts() ) :
-				the_post();
-
-				do_action( 'storefront_page_before' );
-
-				get_template_part( 'content', 'page' );
-
-				/**
-				 * Functions hooked in to storefront_page_after action
-				 *
-				 * @hooked storefront_display_comments - 10
-				 */
-				do_action( 'storefront_page_after' );
-
-			endwhile; // End of the loop.
-			?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
+                ?>
+            </div>
+        </div>
+    </div>
 <?php
 get_footer();
