3de7231b2add15bde975f830674c4ea7bdb9f4c3
mastekator <akmiskam@yandex.ru>
2019-11-18 13:45:02 +0300

    об авторе и правки

--- wp-content/themes/storefront-child/template-parts/about.php
+++ wp-content/themes/storefront-child/template-parts/about.php
@@ -8,10 +8,13 @@ Template Post Type: post, page, product
 <?php get_header(); ?>
 
 
-<div class="container pt-5 pb-5 popular">
+<div class="container popular">
     <div class="row">
-        <div class="col-12">
-            <h1>Страница Об авторе</h1>
+        <div class="col-12"><h2 class="page-title"><?php the_title() ?></h2></div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <?php the_content(); ?>
         </div>
     </div>
 </div>
