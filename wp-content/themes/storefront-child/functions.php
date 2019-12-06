16387d973aba8e83d1a2cf91a3ad888184184a20
Igor Garifyanov <garifianov.i@gmail.com>
2019-12-06 11:26:29 +0300

    Исправление для совместимости версий

--- wp-content/themes/storefront-child/functions.php
+++ wp-content/themes/storefront-child/functions.php
@@ -804,6 +804,16 @@ function bookCardInReader()
     <?php
 }
 
+if (! function_exists("array_key_last")) {
+    function array_key_last($array) {
+        if (!is_array($array) || empty($array)) {
+            return NULL;
+        }
+
+        return array_keys($array)[count($array)-1];
+    }
+}
+
 /**
  * Дополняем хлебные крошки в читалке названием главы
  */
