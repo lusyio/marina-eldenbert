b7c22aa69290164d7a9da44639ab35f641722acb
Igor Garifyanov <garifianov.i@gmail.com>
2019-12-10 10:52:25 +0300

    Исправил цены в корзине

--- wp-content/themes/storefront-child/woocommerce/cart/cart.php
+++ wp-content/themes/storefront-child/woocommerce/cart/cart.php
@@ -74,9 +74,7 @@ do_action( 'woocommerce_before_cart' ); ?>
 
                     <td class="product-subtotal" data-title="<?php esc_attr_e( 'Total', 'woocommerce' ); ?>">
                         <?php
-                        $priceHtml = ($_product->price == 0) ? preg_replace('~&#8381;~', '', WC()->cart->get_product_subtotal( $_product, $cart_item['quantity'] )) : WC()->cart->get_product_subtotal( $_product, $cart_item['quantity'] );
-
-                        echo apply_filters( 'woocommerce_cart_item_subtotal', $priceHtml, $cart_item, $cart_item_key ); // PHPCS: XSS ok.
+                        echo apply_filters( 'woocommerce_cart_item_subtotal', WC()->cart->get_product_subtotal( $_product, $cart_item['quantity'] ), $cart_item, $cart_item_key ); // PHPCS: XSS ok.
                         ?>
                     </td>
 
@@ -124,11 +122,7 @@ do_action( 'woocommerce_before_cart' ); ?>
                 Сумма к оплате
             </td>
             <td class="cart-total">
-                <?php
-                $totals = WC()->cart->get_totals();
-                $totalHtml = ($totals['total'] == 0) ? preg_replace('~&#8381;~', '', WC()->cart->get_total()) : WC()->cart->get_total();
-                ?>
-                <?php echo $totalHtml ?>
+                <?php echo WC()->cart->get_total() ?>
             </td>
             <td class="product-remove">
             </td>
