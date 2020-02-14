2207a578db5af4ec18dfc7f55cfc52620908d5f0
Igor Garifyanov <garifianov.i@gmail.com>
2020-02-14 11:57:55 +0300

    Запретил вставку форматированного текста в поле комментария

--- wp-content/themes/storefront-child/inc/assets/js/custom.js
+++ wp-content/themes/storefront-child/inc/assets/js/custom.js
@@ -79,6 +79,17 @@ jQuery(document).ready(function ($) {
         loadMore('.news-n-events-item', '.load-more', 9);
     }
 
+    $('#nep_fake_textarea').addEventListener("paste", function(e) {
+        // cancel paste
+        e.preventDefault();
+
+        // get text representation of clipboard
+        var text = (e.originalEvent || e).clipboardData.getData('text/plain');
+
+        // insert text manually
+        document.execCommand("insertHTML", false, text);
+    });
+
 });
 
 var swiperPopular = new Swiper('.swiper-container-popular', {
