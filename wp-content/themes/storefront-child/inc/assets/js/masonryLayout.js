913df263e659e68ef8bacb1a03c19f7397ce7055
Igor Garifyanov <garifianov.i@gmail.com>
2020-02-18 13:47:12 +0300

    Доработал раздел Иллюстрации

--- wp-content/themes/storefront-child/inc/assets/js/masonryLayout.js
+++ wp-content/themes/storefront-child/inc/assets/js/masonryLayout.js
@@ -2,8 +2,6 @@ jQuery(function ($) {
     var $grid = $('.grid-images').imagesLoaded(function () {
         $grid.masonry({
             itemSelector: '.grid-item',
-            percentPosition: true,
-            columnWidth: '.grid-sizer'
         });
     });
 });
