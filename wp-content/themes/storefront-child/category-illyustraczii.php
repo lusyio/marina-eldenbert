0dd4f11f1dde987f8223d47bac26fa5e389a5e64
Igor Garifyanov <garifianov.i@gmail.com>
2020-02-19 17:13:37 +0300

    Добавил стили в иллюстрации

--- wp-content/themes/storefront-child/category-illyustraczii.php
+++ wp-content/themes/storefront-child/category-illyustraczii.php
@@ -18,26 +18,33 @@ get_header(); ?>
                     <header class="page-header">
                         <h1 class="page-title"><?php echo $currentCategory->name ?></h1>
                     </header><!-- .page-header -->
-                    <div>
-                        <div class="grid grid-images">
-                            <div class="grid-sizer"></div>
+
                     <?php $subCategories =  get_categories('child_of=' . $cat);
-                    foreach ($subCategories as $subCategory): ?>
-                        <div class=" grid-item<?php echo $categoryClass ?> text-center">
-                            <a data-category="<?php echo $categoryClass ?>" class="album-img images-link"
-                               href="<?php echo get_category_link($subCategory) ?>">
-                                <?php
-                                $tax_term_id = $subCategory->term_taxonomy_id;
-                                $images = get_option('taxonomy_image_plugin');
-                                echo '<img src="' . wp_get_attachment_image_url($images[$tax_term_id], 'full') . '" class="attachment-medium size-medium mb-3">';
-                                ?>
-                                <span><?php echo $subCategory->name?></span>
-                            </a>
+                    $catCount = count($subCategories);
+                    $elCount = 1;
+
+                    foreach ($subCategories as $subCategory):
+                        if ($elCount % 3 == 1):?>
+                        <div class="row">
+                            <?php endif; ?>
+                            <div class="col-lg-4 col-md-6 col-12 text-center mb-4">
+                                <div class="album-card">
+                        <?php
+                        $tax_term_id = $subCategory->term_taxonomy_id;
+                        $images = get_option('taxonomy_image_plugin');
+                        echo '<img src="' . wp_get_attachment_image_url($images[$tax_term_id], 'full') . '" class="attachment-medium size-medium mb-3">';
+                        ?>
+                                <div class="album-name">
+                                    <div class="album-title"><?php echo $subCategory->name?></div>
+                                    <div><?php echo $subCategory->category_count?></div>
+                                </div>
+                            </div>
                         </div>
-                    <?php
-                        endforeach; ?>
-                        </div>
+                        <?php if ($elCount % 3 == 0 || $elCount == $catCount):?>
                     </div>
+                    <?php endif; ?>
+                    <?php $elCount++; ?>
+                    <?php endforeach; ?>
                 </main><!-- #main -->
             </div><!-- #primary -->
         </div>
