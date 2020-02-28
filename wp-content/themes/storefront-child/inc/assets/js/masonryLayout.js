a5015863380b385c0ededcc71cf941517ff6bec3
mastekator <akmiskam@yandex.ru>
2020-02-28 13:50:41 +0300

    имагес

--- wp-content/themes/storefront-child/inc/assets/js/masonryLayout.js
+++ wp-content/themes/storefront-child/inc/assets/js/masonryLayout.js
@@ -2,6 +2,7 @@ jQuery(function ($) {
     var $grid = $('.grid-images').imagesLoaded(function () {
         $grid.masonry({
             itemSelector: '.grid-item',
+            gutter: 30
         });
     });
 });
