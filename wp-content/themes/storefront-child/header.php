7b264f7f07eac0952843fcd4d4d11a48cd09918e
Kelevra <kelevra@MacBook-Pro-Dmitrij-2.local>
2020-03-29 12:36:29 +0300

    Google Tag Manager

--- wp-content/themes/storefront-child/header.php
+++ wp-content/themes/storefront-child/header.php
@@ -19,11 +19,23 @@
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.5.0/css/swiper.min.css">
     <?php wp_head(); ?>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-N4TVJG5');</script>
+    <!-- End Google Tag Manager -->
+
 </head>
 
 <body <?php body_class(); ?>>
 
 <?php do_action('storefront_before_site'); ?>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N4TVJG5"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 
 <div id="page" class="hfeed site">
     <?php do_action('storefront_before_header'); ?>
