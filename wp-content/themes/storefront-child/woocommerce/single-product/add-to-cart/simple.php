80f5e9f883743d3d327f14bda1bc8fde31208598
Igor Garifyanov <garifianov.i@gmail.com>
2020-03-31 11:27:25 +0300

    Убрал кнопку добавить в корзину если товар уже куплен

--- wp-content/themes/storefront-child/woocommerce/single-product/add-to-cart/simple.php
+++ wp-content/themes/storefront-child/woocommerce/single-product/add-to-cart/simple.php
@@ -74,7 +74,7 @@ if ($product->is_in_stock()) : ?>
                     }
                 }
             }
-            if (!$hasDownloads):
+            if (!isBookBought($product->get_id())):
                 ?>
                 <button type="submit" name="add-to-cart" value="<?php echo esc_attr($product->get_id()); ?>"
                         class="single_add_to_cart_button button alt"><?php echo esc_html($product->single_add_to_cart_text()); ?></button>
