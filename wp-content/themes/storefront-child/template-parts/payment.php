ec01d58cf9bd2bf057f3d822b16f3a12c390d4fb
Maxim Tretiak <48452049+mastekator@users.noreply.github.com>
2020-04-06 15:45:15 +0300

    шаблоны для страницы оплата и возврат

---
+++ wp-content/themes/storefront-child/template-parts/payment.php
@@ -0,0 +1,30 @@
+<?php
+/*
+Template Name: payment
+Template Post Type: post, page, product
+*/
+?>
+
+<?php get_header(); ?>
+
+<div class="container pb-5">
+    <div class="row">
+        <div class="col-12">
+            <h2 class="page-title">Оплата</h2>
+            <p>Вы можете оплатить заказ при помощи:</p>
+            <ul>
+                <li>Электронных денег - платёжной системой PayPal</li>
+                <li>Пластиковой картой - оплата происходит через авторизационный сервер Яндекс Кассы с использованием
+                    банковских карт следующих платежных систем: VISA International, MasterCard World Wide.
+                </li>
+                <li>Apple Pay и Google Pay</li>
+            </ul>
+            <p>Наши реквизиты:</p>
+            <p>ИП Литягина Ксения Федоровна</p>
+            <p>ОГРНИП: 319631300175537</p>
+            <p>ИНН: 631941515859</p>
+        </div>
+    </div>
+</div>
+
+<?php get_footer(); ?>
